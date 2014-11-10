63f0388d49b191f63508ee7d3dcbb2036a23d154
Jose Alday Pinheiro Alves <aldaypinheiro@Joses-MacBook-Air.local>
2014-11-10 11:40:28 +0000

    uptdate converting string to number withou atoi and initial code to get timestamp of the system

--- main.c
+++ main.c
@@ -1,31 +1,40 @@
- #include <stdio.h>
- #include <string.h>
- #include <libxml/parser.h>
- #include <libxml/tree.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 
 char *user_input(char *buffer, char *node_name) {
 	printf("Enter the %s:\n", node_name);
-	if (fgets(buffer, 1000, stdin) == NULL) {
+	if (fgets(buffer, 256, stdin) == NULL) {
+		fprintf(stderr, "failed to read string\n");
 		return NULL;
 	}
-	strtok(buffer, "\n");			
+	strtok(buffer, "\n");
 	return buffer;
 }
 
 int main(int argc, char **argv) {
 	int i, j;
-	char buffer[1000];
+	char buffer[256];
+	time_t rawtime;
+	struct tm *info;
+	char *end = NULL;
 	xmlDocPtr doc = NULL;
 	xmlNodePtr root_node = NULL, products = NULL, node = NULL;
 
 	doc = xmlNewDoc(BAD_CAST "1.0");
 	root_node = xmlNewNode(NULL, BAD_CAST "basket");
 	xmlDocSetRootElement(doc, root_node);
-	xmlNewChild(root_node, NULL, BAD_CAST "purchase-timestamp", BAD_CAST "2014-10-10 12:12:12+00:00");
+
+	info = localtime(&rawtime);
+
+	xmlNewChild(root_node, NULL, BAD_CAST "purchase-timestamp", BAD_CAST "2014-10-10 12:12:12+00:00");//strftime(buffer, 80, "%x %X", info));//
 	products = xmlNewChild(root_node, NULL, BAD_CAST "products", NULL);
 	
-	j = atoi(user_input(buffer, "number of products"));
-
+	//j = atoi(user_input(buffer, "number of products"));
+	j = strtoul(user_input(buffer, "number of products"), &end, 10);
+	
 	for(i = 1; i <= j; i++) {
 		node = xmlNewChild(products, NULL, BAD_CAST "product", NULL);
 			
